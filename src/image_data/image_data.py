1216e4234bd26f0c347c728e9319f7d0c5065651
Jacob Berg <jacoberg2@Jacob-Bergs-MacBook-Pro.local>
2013-06-23 19:00:27 -0400

    marked some class variables private

--- src/image_data/image_data.py
+++ src/image_data/image_data.py
@@ -7,7 +7,6 @@ Created on Jun 10, 2013
 
 import os
 import shutil
-import sys
 import pyexiv2
 
 from errno import ENOENT
@@ -27,33 +26,34 @@ class Image_Data:
         '''
         Constructor
         '''
-        self.image_path = path
-        if not os.path.exists(self.image_path) or not os.path.isfile(self.image_path):
-            raise IOError(ENOENT, os.strerror(ENOENT), filename)
+        self._image_path = path
+        if not os.path.exists(self._image_path) or not os.path.isfile(self._image_path):
+            raise IOError(ENOENT, os.strerror(ENOENT), self._image_path)
         
-        self.image_time = time.dst()
-        self.id_tags = []
+        self._image_time = time.dst()
+        self._id_tags = []
         
     def tag_count (self):
-        return len(self.id_tags)
+        return len(self._id_tags)
     
     def move_image_file (self, to_path):
         try:
-            shutil.move(self.image_path, to_path)
+            shutil.move(self._image_path, to_path)
         except:
             raise
     
     def add_id_tag(self,id_tag):
-        self.id_tags.append(id_tag)
+        self._id_tags.append(id_tag)
         
     def extract_image_time(self):
-        metadata = pyexiv2.ImageMetadata(self.image_path)
+        metadata = pyexiv2.ImageMetadata(self._image_path)
         metadata.read()
 
         time_array = (metadata[self._time_key].raw_value).split(' ')
-        self.image_time = datetime.strptime(time_array[1], '%H:%M:%S')
+        self._image_time = datetime.strptime(time_array[1], '%H:%M:%S')
+        return self._image_time
         
     def attach_id_tags(self):
-        metadata = pyexiv2.ImageMetadata(self.image_path)
-        metadata[self._keyword_key] = pyexiv2.IptcTag(self.keyword_key, self.id_tags)
+        metadata = pyexiv2.ImageMetadata(self._image_path)
+        metadata[self._keyword_key] = pyexiv2.IptcTag(self._keyword_key, self._id_tags)
         metadata.write(preserve_timestamps=True)
